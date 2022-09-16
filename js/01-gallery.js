5aa43c5298396e8f68f3e2ebc8129b5f0ec13dd7
YuriySamusew <6177385@gmail.com>
2022-09-16 23:02:53 +0300

    First task ended

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryMarkup(galleryItems);
+// console.log(createGalleryMarkup(galleryItems));
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  }).join('');
+  
+  // console.log(markup);  
+}
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick (evt) {
+    // console.log(evt.target);
+    evt.preventDefault()
+
+    // const isGalleryItemEl = evt.target.classList.contains('gallery__image');
+    // if(!isGalleryItemEl) {
+    //     return;
+    // }
+
+    if(evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+    // const originalImgEl = evt.target.dataset.source;
+    // console.log(originalImgEl);
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" wtdth = "800" height = "600"/>
+`);
+instance.show();
+
+galleryContainer.addEventListener("keydown", (evt) => {
+    if(evt.code === "Escape") {
+        instance.close();
+    }
+});
+
+}
